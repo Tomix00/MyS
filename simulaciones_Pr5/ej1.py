edf6fd0ec802ec7807ca1c64b31225dbaf3b930b
Tomix00 <tomi.code@hotmail.com>
2026-05-13 23:32:14 +0000

    Implemented ej1 and ej2 for Pr5

--- simulaciones_Pr5/ej1.py
+++ simulaciones_Pr5/ej1.py
@@ -1,2 +1,23 @@
 import math
 from random import random
+
+def densidad_a():
+    U = random()
+    if U < 0.25:
+        return 2 + 2 * math.sqrt(U)
+    else:
+        return 6 - 2 * math.sqrt(3 * (1 - U))
+
+def densidad_b():
+    U = random()
+    if U < 0.6:
+        return -3 + math.sqrt(9 + 35 * U / 3)
+    else:
+        return ((35 * U -19) / 2) ** (1/3)
+
+def densidad_c():
+    U = random()
+    if U < 1/16:
+        return 0.25 * math.sqrt(16 * U)
+    else:
+        return 4 * U - 0.25
---
+++ simulaciones_Pr5/ej2.py
@@ -0,0 +1,43 @@
+from transf_inversa import continousGenerators as cg
+from random import random
+import math
+
+# ----- Ejercicio 2-a -----
+def pareto(a):
+    U = random()
+    return U ** (-1/a)
+
+def erlang(k: int, mu: float):
+    lam = 1 / mu
+    suma = 0
+    for _ in range(k):
+        suma += cg.exponentialL(lam)
+    return suma
+
+def weibull(lam, beta):
+    U = random()
+    return lam * (-math.log(U)) ** (1/beta)
+
+# ----- Ejercicio 2-b -----
+def estimacion():
+    n = 10000
+
+    sum_pareto = 0
+    sum_erlang = 0
+    sum_weibull = 0
+
+    for _ in range(n):
+        sum_pareto += pareto(2)
+        sum_erlang += erlang(2, 2)
+        sum_weibull += weibull(1, 2)
+    
+    media_pareto = sum_pareto / n
+    media_erlang = sum_erlang / n
+    media_weibull = sum_weibull / n
+
+    print(f"Pareto: {media_pareto:.4f}")
+    print(f"Erlang: {media_erlang:.4f}")
+    print(f"Weibull: {media_weibull:.4f}")
+
+if __name__ == "__main__":
+    estimacion()
